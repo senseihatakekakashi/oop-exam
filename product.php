4308567dce02b93c9ebfa88ca7465a1634ba478a
digexperience <jolly0903992@gmail.com>
2023-02-02 19:18:44 +0800

    product

---
+++ product.php
@@ -0,0 +1,24 @@
+<?php 
+
+Class Product
+	private $name
+	private $description
+	private $price
+	private $srp
+	
+	function setName()
+
+	function setDescription()
+
+	function setPrice()
+	
+	function getName()
+
+	function getDescription()
+    
+	function getPrice()
+	
+	function computeSRP() {
+		price * 1.5;
+	}
+?>
